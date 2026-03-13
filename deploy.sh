8b17cab05fe0da1f89b50f1935964f243a825e22
luiz.ricardo <luiz.ricardo@MacBook-Air-de-admininfra.local>
2026-03-13 09:47:14 -0300

    test pipe

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,8 @@ set -Eeuo pipefail
 echo "Imagem:"
 echo "${ACR_LOGIN_SERVER}/${IMAGE_NAME}:${IMAGE_TAG}"
 
-docker compose config -q
+
+docker compose config -q    
 docker compose pull || true
 docker compose up -d --remove-orphans
 docker compose ps
