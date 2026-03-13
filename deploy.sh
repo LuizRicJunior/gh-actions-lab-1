e06e333a4cb7e3b3dd468f26e9841a1e5ed8b62b
luiz.ricardo <luiz.ricardo@MacBook-Air-de-admininfra.local>
2026-03-12 22:40:13 -0300

    Modificacoes

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,10 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail
 
-APP_DIR="/opt/deploy/lab"
-
-cd $APP_DIR
-
 echo "Imagem:"
 echo "${ACR_LOGIN_SERVER}/${IMAGE_NAME}:${IMAGE_TAG}"
 
 docker compose config -q
-
 docker compose pull || true
-
 docker compose up -d --remove-orphans
-
 docker compose ps
